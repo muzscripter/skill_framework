9c6387bc943f120c3dd80b6b6df9333d0972c70c
graverealm <109730545+muzscripter@users.noreply.github.com>
2023-08-13 09:06:33 -0400

    Update skill_replicator.lua

--- skill_replicator.lua
+++ skill_replicator.lua
@@ -1,30 +1,41 @@
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local UserInputService = game:GetService('UserInputService')
 
+local SFX = ReplicatedStorage.Storage.SFX
 local Cooldown = require(ReplicatedStorage.Storage.Modules.Cooldown)
 local Player = game.Players.LocalPlayer
+local Character = Player.Character
 
 local GetData = ReplicatedStorage.Storage.Remotes.GetData
 
-local function UseSkill(skillName)
-	print("Using skill:", skillName)
-	
-	if skillName == 'Water Basin' then
-		local Water = require(ReplicatedStorage.Storage.Modules.Moves.Water)
-		
-		Water['WaterBasin'](Player.Character)
-	end
-end
-
-
-UserInputService.InputBegan:Connect(function(ClientInput, GameProcessed)
-	if GameProcessed then return end
+local function UseSkill(Skill)
+	local Moves = ReplicatedStorage.Storage.Modules.Moves
 	
 	local SkillTable = GetData:InvokeServer(Player)
 	
-	for Skill, SkillSet in pairs(SkillTable) do
-		if ClientInput.KeyCode == Enum.KeyCode[SkillSet[Skill].KeyBind] then
-			UseSkill(Skill)
+	local MoveSet = require(Moves[SkillTable.Skillset])
+
+	if MoveSet[Skill] then
+		MoveSet[Skill](Player.Character)
+	end
+end
+
+UserInputService.InputBegan:Connect(function(ClientInput, GameProcessed)
+	if GameProcessed then return end
+
+
+
+	local SkillTable = GetData:InvokeServer(Player)
+	
+	if SkillTable then
+		for Skill, SkillSet in pairs(SkillTable.Skills) do
+			if ClientInput.KeyCode == Enum.KeyCode[SkillSet[Skill].KeyBind] then
+				print("KeyBind:", SkillSet[Skill].KeyBind)
+				print(string.format("Skill Used '%s' ", Skill))
+				UseSkill(Skill)
+			end
 		end
+	else
+		print("SkillTable is nil")
 	end
 end)
